feea913c7a75faffd879dc894ae4d74ba06ec238
semedii <abdisamed2023@hotmail.com>
2025-03-21 19:14:36 +0300

    refactored login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -16,103 +16,134 @@ class LoginPage extends StatelessWidget {
         create: (context) => LoginCubit(),
         child: BlocListener<LoginCubit, LoginState>(
           listener: _getListener,
-          child: BlocBuilder<LoginCubit, LoginState>(builder: (context, state) {
-            if (state is LoginInitial) {
-              return SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: MediaQuery.of(context).size.height * 0.4,
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 232, 241, 249),
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(60),
-                          bottomRight: Radius.circular(60),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 4,
-                            offset: Offset(0, 2),
-                          ),
-                        ],
-                      ),
-                      child: Image.asset(
-                        "assets/images/logo.png",
-                        scale: 2,
-                      ),
-                    ),
-                    SizedBox(height: 24),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: _buildLoginForm(context, state),
-                    ),
-                  ],
-                ),
-              );
-            }
-            if (state is LoginLoading) {
-              return Scaffold(
-                body: Center(
-                  child: CircularProgressIndicator(
-                    color: Color(0xFF1e78c1),
-                  ),
-                ),
-              );
-            }
-            return SizedBox.shrink();
-          }),
+          child: BlocBuilder<LoginCubit, LoginState>(
+            builder: _mapStateToWidget,
+          ),
         ),
       ),
     );
   }
 
+  Widget _mapStateToWidget(context, state) {
+    if (state is LoginInitial) {
+      return _buildLoginForm(context, state);
+    }
+    if (state is LoginLoading) {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            color: Color(0xFF1e78c1),
+          ),
+        ),
+      );
+    }
+    return SizedBox.shrink();
+  }
+
   Widget _buildLoginForm(BuildContext context, LoginInitial state) {
     LoginCubit cubit = BlocProvider.of<LoginCubit>(context);
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.5,
+    return SingleChildScrollView(
       child: Column(
         children: [
+          _buildTopContainer(context),
+          SizedBox(height: 24),
           Padding(
-            padding: const EdgeInsets.only(bottom: 12),
-            child: Text(
-              "Login",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: Column(
+                children: [
+                  _buildLoginText(),
+                  _buildEmailTextField(cubit),
+                  _buildPasswordTextField(state, cubit),
+                  Spacer(),
+                  _buildLoginButton(cubit)
+                ],
               ),
             ),
           ),
-          AppTextFormField(
-            label: "Email",
-            prefixIcon: Icons.person,
-            onChanged: cubit.onEmailChanged,
-          ),
-          AppTextFormField(
-            label: "Password",
-            prefixIcon: Icons.lock,
-            isObscure: state.isPasswordObscure,
-            suffixWidget: GestureDetector(
-              onTap: cubit.onIsPasswordObscureChanged,
-              child: Icon(
-                  state.isPasswordObscure
-                      ? Icons.visibility_off
-                      : Icons.visibility,
-                  color: const Color(0xFF1e78c1)),
-            ),
-            onChanged: cubit.onPasswordChanged,
-          ),
-          Spacer(),
-          AppButton(
-            text: "Login",
-            onPressed: cubit.onLogin,
-            color: Colors.white,
-            fontColor: Color(0xFF1e78c1),
-          )
         ],
       ),
+    );
+  }
+
+  Container _buildTopContainer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.4,
+      decoration: _buildTopContainerDecoration(),
+      child: _buildLogo(),
+    );
+  }
+
+  BoxDecoration _buildTopContainerDecoration() {
+    return BoxDecoration(
+      color: const Color.fromARGB(255, 232, 241, 249),
+      borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(60),
+        bottomRight: Radius.circular(60),
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black12,
+          blurRadius: 4,
+          offset: Offset(0, 2),
+        ),
+      ],
+    );
+  }
+
+  Image _buildLogo() {
+    return Image.asset(
+      "assets/images/logo.png",
+      scale: 2,
+    );
+  }
+
+  Padding _buildLoginText() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: Text(
+        "Login",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  AppTextFormField _buildEmailTextField(LoginCubit cubit) {
+    return AppTextFormField(
+      label: "Email",
+      prefixIcon: Icons.person,
+      onChanged: cubit.onEmailChanged,
+    );
+  }
+
+  AppTextFormField _buildPasswordTextField(
+      LoginInitial state, LoginCubit cubit) {
+    return AppTextFormField(
+      label: "Password",
+      prefixIcon: Icons.lock,
+      isObscure: state.isPasswordObscure,
+      suffixWidget: GestureDetector(
+        onTap: cubit.onIsPasswordObscureChanged,
+        child: Icon(
+            state.isPasswordObscure ? Icons.visibility_off : Icons.visibility,
+            color: const Color(0xFF1e78c1)),
+      ),
+      onChanged: cubit.onPasswordChanged,
+    );
+  }
+
+  AppButton _buildLoginButton(LoginCubit cubit) {
+    return AppButton(
+      text: "Login",
+      onPressed: cubit.onLogin,
+      color: Colors.white,
+      fontColor: Color(0xFF1e78c1),
     );
   }
 
